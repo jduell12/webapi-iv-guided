d604c28d2baae1cc9044256c5f543836af6c04c6
Jessica <duellje@oregonstate.edu>
2020-08-05 21:19:55 -0400

    working api

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const server = require("./api/server.js");
 
 const port = process.env.PORT || 5000;
 
-server.listen(prompt, () => {
+server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
